0f99e5127fe175d62f4c5427dfea2f65d0472b5b
YuraSS23 <nikitsenko_yura@mail.ru>
2024-11-14 09:39:35 +0300

    deploy

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from "next";
 import {Header} from "@/components/shared";
+import {Suspense} from "react";
 
 export const metadata: Metadata = {
     title: "Next Pizza | Главная",
@@ -7,13 +8,18 @@ export const metadata: Metadata = {
 
 export default function HomeLayout({
                                        children,
+                                       modal
                                    }: Readonly<{
     children: React.ReactNode;
+    modal: React.ReactNode;
 }>) {
     return (
         <main className={'min-h-screen'}>
-            <Header/>
+            <Suspense>
+                <Header/>
+            </Suspense>
             {children}
+            {modal}
         </main>
     );
 }
